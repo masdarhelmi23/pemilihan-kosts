257835ecbe6441ee15b877cf4b1cc85623693e2e
unknown <masdarhelmi23@gmail.comgit config --global user.email masdarhelmi23@gmail.com>
2025-07-09 16:21:13 +0700

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Kost Showcase')</title>
-    <link href="{{ asset('bulma/css/bulma.min.css') }}" rel="stylesheet">
+    <!-- Bulma CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" rel="stylesheet">
 </head>
 <body>
     <section class="section">
